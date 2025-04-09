33f7d751187636aea7b3adfbe4c5705eee4330e4
Shreyansh Agrawal <f20140324h@alumni.bits-pilani.ac.in>
2025-04-09 18:08:10 -0500

    bug fixes

--- main.py
+++ main.py
@@ -7,14 +7,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 import time
 from datetime import date, timedelta
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # Set up Chrome WebDriver (you can replace this with another browser driver like Firefox if needed)
 
 ### headless options
 options = Options()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
+# options.add_argument('--headless')
+# options.add_argument('--no-sandbox')
+# options.add_argument('--disable-dev-shm-usage')
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 print("DRIVER INITIALIZED")
@@ -33,84 +35,95 @@ next_week = next_week.strftime("%m/%d/%Y")
 
 
 # login
-driver.find_element(By.XPATH,'//*[@id="login"]').send_keys(u_name)
-driver.find_element(By.XPATH,'//*[@id="password"]').send_keys(password)
-driver.find_element(By.XPATH,'//*[@id="loginButton"]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="login"]'))).send_keys(u_name)
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="password"]'))).send_keys(password)
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="loginButton"]'))).click()
 
-time.sleep(3)
+#time.sleep(3)
 print("LOGGED IN")
 
 # # go to reservations
-# driver.find_element(By.XPATH,'//*[@id="menu_reserve_a_court"]').click()
+# WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="menu_reserve_a_court"]').click()
 
 # # Select "Gym"
-# driver.find_element(By.XPATH,'//*[@id="component_chosen"]').click()
-# driver.find_element(By.XPATH,'//*[@id="component_chosen"]/div/ul/li[1]').click()
+# WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="component_chosen"]').click()
+# WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="component_chosen"]/div/ul/li[1]').click()
+
+WebDriverWait(driver, 10).until(
+EC.visibility_of_element_located((By.XPATH, '//*[@id="menu_reserve_a_court"]'))
+).click()
 
 
 # go to reservations
-driver.find_element(By.XPATH,'//*[@id="menu_reserve_a_court"]').click()
+#WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="menu_reserve_a_court"]').click()
 print("step 2")
 
 # Select "Gym"
-driver.find_element(By.XPATH,'//*[@id="component_chosen"]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="component_chosen"]'))).click()
 print("step 3")
 
-driver.find_element(By.XPATH,'//*[@id="component_chosen"]/div/ul/li[1]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="component_chosen"]/div/ul/li[1]'))).click()
 time.sleep(2)
 print("step 4")
 
-driver.find_element(By.XPATH,'//*[@id="location_chosen"]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="location_chosen"]'))).click()
 print("step 5")
-driver.find_element(By.XPATH,'//*[@id="location_chosen"]/div/ul/li[4]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="location_chosen"]/div/ul/li[4]'))).click()
 print("step 6")
-time.sleep(5)
+#time.sleep(5)
 
 
 #selecting date
+time.sleep(2)
 driver.find_element(By.XPATH,'//*[@id="date"]').clear()
 print("step 7")
 driver.find_element(By.XPATH,'//*[@id="date"]').send_keys(next_week)
 print("step 8")
 
 #90 mins
-driver.find_element(By.XPATH,'//*[@id="reserve-court-filter"]/div[3]/div[7]/div[3]/div/div/label[3]/span').click()
-#driver.find_element(By.XPATH,'//*[@id="reserve-court-filter"]/div[3]/div[7]/div[3]/div/div/label[1]/span').click() #for 30 mins, change to 2 for 6 mins
+#WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="reserve-court-filter"]/div[3]/div[7]/div[3]/div/div/label[3]/span'))).click()
+driver.find_element(By.XPATH,'//*[@id="reserve-court-filter"]/div[3]/div[7]/div[3]/div/div/label[1]/span' ).click()
+#WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="reserve-court-filter"]/div[3]/div[7]/div[3]/div/div/label[1]/span').click() #for 30 mins, change to 2 for 6 mins
 print("step 9")
 
 #search click
-driver.find_element(By.XPATH,'//*[@id="reserve-court-search"]').click()
+WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="reserve-court-search"]'))).click()
 print("step 10")
-time.sleep(2)
+#time.sleep(2)
 
-# driver.find_element(By.XPATH,'//*[@id="timeFrom_chosen"]').click()
+# WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="timeFrom_chosen"]').click()
 
 ## select slot time
 slot_times = driver.find_elements(By.XPATH,'//*[@id="times-to-reserve"]/tbody/tr/td/a')
 print("step 11")
 
-to_book = ['5:30pm', '6:00pm', '6:30pm']
+to_book = ['6:00pm', '6:30pm', '5:30pm','9:30pm']
 
-# booking slot
-for e in slot_times:
-    slot_time = e.get_attribute('text')
-    slot_time = slot_time.strip()
+if len(slot_times)>0:
 
-    print(slot_time)
+    # booking slot
+    for e in slot_times:
+        slot_time = e.get_attribute('text')
+        slot_time = slot_time.strip()
 
-    if slot_time in to_book:
-        e.click()
-        
-        # final reservation click
-        time.sleep(3)
-        driver.find_element(By.XPATH,'//*[@id="confirm"]').click()
-        print("step final")
-        print("Badminton slot booked: ", slot_time)
-        
-        break
+        print(slot_time)
 
-    else:
-        print("No relevant slots found")
+        if slot_time in to_book:
+            e.click()
+            
+            # final reservation click
+            #time.sleep(3)
+            WebDriverWait(driver, 10).until( EC.visibility_of_element_located((By.XPATH,'//*[@id="confirm"]'))).click()
+            print("step final")
+            print("Badminton slot booked: ", slot_time)
+            
+            break
+
+        else:
+            print("No relevant slots found")
+
+else:
+    print("No slots found")
     
 ####
-driver.quit()
+#driver.quit()
