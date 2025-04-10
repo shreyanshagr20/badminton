5390b66f80afae433c0102556bb391105ad21a91
Shreyansh Agrawal <f20140324h@alumni.bits-pilani.ac.in>
2025-04-09 19:18:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ options = Options()
 options.add_argument('--headless=new')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 # service = Service('/usr/bin/chromedriver')  # path to your installed chromedriver
