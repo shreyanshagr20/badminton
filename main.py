8b7e69972fd41342d751539a70d24b6e8ddae984
Shreyansh Agrawal <f20140324h@alumni.bits-pilani.ac.in>
2025-04-15 16:40:09 -0500

    test monday

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ u_name = 'sagrawal'
 password = 'Myfitnesspass01@'
 
 #date time
-next_week = date.today() + timedelta(days=7)
+next_week = date.today() + timedelta(days=6) # make it default as 7
 next_week = next_week.strftime("%m/%d/%Y")
 
 
